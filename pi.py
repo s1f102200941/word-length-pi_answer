484e672dacdf9d34a5241d9c09cf9cb472f920d5
s1f102200941 <s1f102200941@iniad.org>
2024-10-15 17:34:01 +0900

    Answer question

--- pi.py
+++ pi.py
@@ -1,4 +1,7 @@
 def get_pi_digits():
     text = "How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard."
-
-    # ここに実装を書く
+    cleaned_text = text.replace(",", "").replace(".", "")
+    word_list = cleaned_text.split()
+    numbers=list(map(len, word_list))
+    result = int(''.join(map(str, numbers)))
+    return result
